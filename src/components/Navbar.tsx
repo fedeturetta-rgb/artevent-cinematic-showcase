3441795c2367b456b46577db1aa83464a3473463
fedeturetta-rgb <fedeturetta@gmail.com>
2026-03-13 23:47:54 +0100

    dimensione logo 8 era 24

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,8 @@ const navLinks = [
 const logoImagePath = "/images/ARTEVENT2_2.png";
 
 // dimensioni logo (mobile + desktop)
-const logoMobileClass = "h-24";
-const logoDesktopClass = "md:h-24";
+const logoMobileClass = "h-8";
+const logoDesktopClass = "md:h-8";
 
 // dimensioni pulsante CTA (mobile + desktop)
 const ctaMobileClass = "text-[8px]"; // esempio: h-6 testo piccolissimo
