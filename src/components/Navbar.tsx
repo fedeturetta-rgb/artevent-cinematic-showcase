4158cdafbb5a344dc4b765d88cfe49864d787a00
fedeturetta-rgb <fedeturetta@gmail.com>
2026-03-13 23:39:22 +0100

    altezza loghi 99 era 24

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,8 @@ const navLinks = [
 const logoImagePath = "/images/ARTEVENT2_2.png";
 
 // dimensioni logo (mobile + desktop)
-const logoMobileClass = "h-24";
-const logoDesktopClass = "md:h-24";
+const logoMobileClass = "h-99";
+const logoDesktopClass = "md:h-99";
 
 // dimensioni pulsante CTA (mobile + desktop)
 const ctaMobileClass = "text-[8px]"; // esempio: h-6 testo piccolissimo
