ed1d11f1a02f4823a263afe2d000111ba1bc7d98
fedeturetta-rgb <fedeturetta@gmail.com>
2026-03-13 17:39:25 +0100

    32

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar = () => {
               className="h-80 md:h-36 w-auto object-contain"
             />
           ) : (
-            <Logo size={80} />
+            <Logo size={32} />
           )}
         </a>
 
