57dad42eb5724c853e52037b7a5d845fa677ed6a
fedeturetta-rgb <fedeturetta@gmail.com>
2026-03-28 21:18:41 +0100

    Change base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Artevent-Production/",
+  base: "./",
   
   server: {
     host: "::",
